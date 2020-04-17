d0b403cb1d69d21b54f5a5b36ed2fbf39fa511c3
nga <kimnganguyen0212@gmail.com>
2020-04-17 17:29:41 +0700

    v1.3

--- app/src/main/java/vn/edu/ntu/kimnga/nguyenthikimnga_59131500_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/kimnga/nguyenthikimnga_59131500_lifecycle/MainActivity.java
@@ -4,31 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtTG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle", "onCreate Called");
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("LifeCycle", "onStart Called");
+
+
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("LifeCycle", "onRestart Called");
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("LifeCycle", "onResume Called");
+        Toast.makeText(MainActivity.this, "OnResume", Toast.LENGTH_SHORT).show();
+        txtTG = findViewById(R.id.txtTG);
+        SimpleDateFormat spF = new SimpleDateFormat("HH:mm:ss");
+        String date = spF.format(new Date());
+        txtTG.setText(date);
     }
 
     @Override
